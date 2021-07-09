3e9695f943dbb25fcf2d35281196f754d322fe4e
cuboy2020 <65587688+cuboy2020@users.noreply.github.com>
2021-07-09 18:16:04 +0700

    update Fragment 2

--- app/src/main/java/com/group6/noteapp/controller/RegisterFragment02.java
+++ app/src/main/java/com/group6/noteapp/controller/RegisterFragment02.java
@@ -3,22 +3,32 @@ package com.group6.noteapp.controller;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.group6.noteapp.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +39,8 @@ public class RegisterFragment02 extends Fragment {
 
     View inflatedView;
     private FirebaseAuth mAu;
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -72,25 +84,53 @@ public class RegisterFragment02 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-//        inflatedView = inflater.inflate(R.layout.fragment_register02, container, false);
-//        MaterialButton btnNext = inflatedView.findViewById(R.id.btnNext01);
-//        btnNext.setOnClickListener(new View.OnClickListener() {
-//            @Override public void onClick(View v) {
-//                /* Get EditText Views */
-//
-//                TextInputLayout etFullname = inflatedView.findViewById(R.id.textInputRegFullname);
-//                TextInputLayout etBirthday = inflatedView.findViewById(R.id.textInputRegBirthday);
-//                TextInputLayout etAddress = inflatedView.findViewById(R.id.textInputRegAddress);
-//
-//                /* Create local variables to store the EditText Views' current values */
-//
-//                String regFullname = etFullname.getEditText().getText().toString();
-//                String regBirthday = etBirthday.getEditText().getText().toString();
-//                String regAddress = etAddress.getEditText().getText().toString();
-//            }
-//        });
 
-        return inflater.inflate(R.layout.fragment_register02, container, false);
+        // Inflate the layout for this fragment
+        inflatedView = inflater.inflate(R.layout.fragment_register02, container, false);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        /* Get EditText Views */
+
+        TextInputLayout etFullname = inflatedView.findViewById(R.id.textInputRegFullname);
+        TextInputLayout etBirthday = inflatedView.findViewById(R.id.textInputRegBirthday);
+        TextInputLayout etAddress = inflatedView.findViewById(R.id.textInputRegAddress);
+
+        MaterialButton btnNext = inflatedView.findViewById(R.id.btnNext01);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+
+                /* Create local variables to store the EditText Views' current values */
+
+                String regFullname = etFullname.getEditText().getText().toString();
+                String regBirthday = etBirthday.getEditText().getText().toString();
+                String regAddress = etAddress.getEditText().getText().toString();
+                // Create a new user with a first and last name
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", "Ada");
+                user.put("last", "Lovelace");
+                user.put("born", 1815);
+
+                // Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
+
+        return inflatedView;
     }
 }
