c2e41382ed55258e2569b0b8be949962f36d4c69
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-07-09 10:13:51 +0700

    Update ValidationUtils.java

--- app/src/main/java/com/group6/noteapp/util/ValidationUtils.java
+++ app/src/main/java/com/group6/noteapp/util/ValidationUtils.java
@@ -22,15 +22,15 @@ public final class ValidationUtils {
             "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" +
             "|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:" +
             "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$";
-    private final String phoneRegex = "^(0[1-9][0-9]{8})|(\\+84[1-9][0-9]{8})$";
-    private final SimpleDateFormat dateFormatter;   // date formatter
+    private final String phoneRegex = "^(0[1-9][0-9]{8})|(\\+84[1-9][0-9]{8})$";    // RFC 5322
+    private final SimpleDateFormat dateFormatter;                                   // date formatter
 
     /**
      * Constructor
      */
     public ValidationUtils(){
-        this.dateFormatter = new SimpleDateFormat("dd/MM/yyyy");    // instantiate with pattern
-        dateFormatter.setLenient(false);                                   // set lenient to false
+        this.dateFormatter = new SimpleDateFormat("dd/MM/yyyy");        // instantiate with pattern
+        dateFormatter.setLenient(false);                                       // set lenient to false
     }
 
     /**
