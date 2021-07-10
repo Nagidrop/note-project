ee283ed5a77bf61fd4dca6803519eeda8cb6915c
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-07-10 17:58:02 +0700

    Add log out confirmation

--- app/src/main/java/com/group6/noteapp/controller/LoginFragment.java
+++ app/src/main/java/com/group6/noteapp/controller/LoginFragment.java
@@ -244,7 +244,7 @@ public class LoginFragment extends Fragment {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        if (currentUser != null) {
+        if (currentUser != null && currentUser.isEmailVerified()) {
             reload();
         }
     }
