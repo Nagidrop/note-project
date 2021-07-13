5305a9a9d3e723c60ee61de42eb2d2c143e8fdb4
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-07-13 20:07:49 +0700

    Note View Edit Activity partial

--- app/src/main/java/com/group6/noteapp/controller/ViewNoteActivity.java
+++ app/src/main/java/com/group6/noteapp/controller/ViewNoteActivity.java
@@ -1,7 +1,11 @@
 package com.group6.noteapp.controller;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.group6.noteapp.R;
@@ -14,5 +18,33 @@ public class ViewNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_note);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_view_note, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_undo:
+                Toast.makeText(this, "Click Undo Icon.", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.nav_redo:
+                Toast.makeText(this, "Click Redo Icon.", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.nav_menu:
+                Toast.makeText(this, "Click Menu Icon.", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
