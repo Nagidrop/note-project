6348ea15f3b44660296104fae40a2368105cdb85
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-07-16 20:52:14 +0700

    Partial Notebook

--- app/src/main/java/com/group6/noteapp/controller/MainActivity.java
+++ app/src/main/java/com/group6/noteapp/controller/MainActivity.java
@@ -176,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
                                 fragment = new HomeFragment();
                                 break;
                             case R.id.menu_notebooks:
-                                fragment = new HomeFragment();
+                                fragment = new NotebookFragment();
                                 break;
                             case R.id.menu_trash:
                                 fragment = new TrashFragment();
